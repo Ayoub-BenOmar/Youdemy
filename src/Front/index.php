76ac54e55ae763fc4c1bfd494d8466a0b3adeeee
ayuub <ayoubbenomar1230@gmail.com>
2025-01-14 22:30:49 +0100

    create login file

--- src/Front/index.php
+++ src/Front/index.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Register</h1>
+    <!-- <h1>Register</h1>
         <form action="../Handlers/register.php" method="post">
             <label for="nom">Nom:</label>
             <input type="text" id="nom" name="nom" required>
@@ -18,8 +18,8 @@
             <input type="password" id="password" name="password" required>
             <br>
             <button type="submit">Register</button>
-        </form>
-<!-- 
+        </form> -->
+
         <h1>Login</h1>
         <form action="../Handlers/login.php" method="post">
             <label for="email">Email:</label>
@@ -29,7 +29,7 @@
             <input type="password" id="password" name="password" required>
             <br>
             <button type="submit">Login</button>
-        </form> -->
+        </form>
 
 </body>
 </html>
