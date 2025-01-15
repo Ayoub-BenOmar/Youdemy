32fd26979d0ab476dd123b9aec3dfd647fde62bf
ayuub <ayoubbenomar1230@gmail.com>
2025-01-15 15:21:38 +0100

    created interface for login and register

--- src/Handlers/register.php
+++ src/Handlers/register.php
@@ -6,9 +6,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nom = $_POST['nom'];
     $email = $_POST['email'];
     $password = $_POST['password'];
+    $role = $_POST['role'];
 
     try {
-        User::signup($nom, $email, $password);
+        User::signup($nom, $email, $password, $role);
         echo "nik";
         exit();
     } catch (Exception $e) {
